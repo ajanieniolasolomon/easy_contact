e1c310bf3e23601530807f38f37208320e39d817
ajanieniolasolomon <eniolsolomon@gmail.com>
2018-07-02 08:03:58 +0100

    3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'app';
+  loadingRouteConfig: boolean;
+  constructor (private router: Router) {}
+  ngOnInit () {
+    this.router.events.subscribe(event => {
+        if (event instanceof RouteConfigLoadStart) {
+            this.loadingRouteConfig = true;
+        } else if (event instanceof RouteConfigLoadEnd) {
+            this.loadingRouteConfig = false;
+        }
+    });
+}
 }
